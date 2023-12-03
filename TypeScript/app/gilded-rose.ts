96f7e2b17e4362b938a7fedf16c5db44b9970242
Maximiliano Chereza <maximiliano@neromotion.co.nz>
2023-12-03 13:29:04 +1300

    feat: Introducing Conjured item

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -42,19 +42,23 @@ export class GildedRose {
           }
         }
       }
+
       if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+        this.items[i].sellIn = this.items[i].sellIn - 1
       }
+
       if (this.items[i].sellIn < 0) {
         if (this.items[i].name != 'Aged Brie') {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+              if (this.items[i].name == 'Conjured Mana Cake') {
+                this.items[i].quality = Math.max(0, this.items[i].quality - 1)
+              } else if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
                 this.items[i].quality = this.items[i].quality - 1
               }
             }
           } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+            this.items[i].quality = 0
           }
         } else {
           if (this.items[i].quality < 50) {
